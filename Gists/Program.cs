b3ac2de118f2dbfa07d5514c42cbc6e6da242e6b
MaxGuidry <mguidry7@outlook.com>
2017-01-05 09:33:02 -0600

    added Gist Project
    added a seperate project for gists and finished the first gist
    assignment

---
+++ Gists/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gists
+{
+    
+
+    class Program
+    {
+        static void printEvenNumbers(ref List<int> list)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    list.Add(i);
+                }
+              
+            }
+            foreach(int num in list)
+            {
+                Console.WriteLine(num);
+            }
+            for(int i=1;i<=100;i++)
+            {
+                if (i % 10 == 0)
+                {
+                    list.Remove(i);
+                }
+                
+            }
+            foreach(int num in list)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+            printEvenNumbers(ref intList);
+
+            Console.Read();
+            
+        }
+    }
+    
+}
+

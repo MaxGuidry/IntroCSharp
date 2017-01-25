9bb163fe0ca116b0c77c36afb881a2d91a6e596f
MaxGuidry <mguidry7@outlook.com>
2017-01-25 16:17:14 -0600

    testing dictionaries and making a binary reversing program

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        class Student
+        {
+            public Student() { }
+            public Student(string name,string id)
+            {
+                m_Name = name;
+                m_id = id;
+   
+            }
+            private string m_id;
+            private string m_Name;
+        }
+    
+        static void Main(string[] args)
+        {
+            Student Max = new Student("Max", "mg1131");
+            Student Donray = new Student("Doray", "dw1178");
+            Dictionary<string, Student> tester = new Dictionary<string, Student>();
+            tester.Add("Max", Max);
+            tester.Add("Donray", Donray);
+            Student temp = tester["Max"];
+            Console.Read();
+        }
+    }
+}
--- Gists/Program.cs
+++ Gists/Program.cs
@@ -8,6 +8,44 @@ namespace Gists
 {
     class Program
     {
+        static public char Invert(char s)
+        {
+            return ((s == '0') ? '1' : '0');
+        }
+
+        public class Gene
+        {
+            public Gene(string s)
+            {
+                if (s.Length != 8)
+                    throw new Exception("too big or too small... must be 8 long");
+                value = s;
+                tail = value.Substring(4);
+                head = value.Remove(4, 4);
+            }
+            string value;
+            string tail;
+            string head;
+            public override string ToString()
+            {
+                return value;
+            }
+            public string Head
+            {
+                get
+                {
+                    return head;
+                }
+            }
+
+            public string Tail
+            {
+                get
+                {
+                    return tail;
+                }
+            }
+        }
         static void printEvenNumbers(ref List<int> list)
         {
             for (int i = 1; i <= 100; i++)
@@ -16,34 +54,52 @@ namespace Gists
                 {
                     list.Add(i);
                 }
-              
+
             }
-            foreach(int num in list)
+            foreach (int num in list)
             {
                 Console.WriteLine(num);
             }
-            for(int i=1;i<=100;i++)
+            for (int i = 1; i <= 100; i++)
             {
                 if (i % 10 == 0)
                 {
                     list.Remove(i);
                 }
-                
+
             }
-            foreach(int num in list)
+            foreach (int num in list)
             {
                 Console.WriteLine(num);
             }
         }
+        static string BinaryStringThing(string s)
+        {
+            Gene g = new Gene(s);
+
+            string str = "";
+
+            foreach(var v in g.Tail)
+            {
+                str += Invert(v);
+            }
+            str += g.Tail;
+            return str;
+        }
         static void Main(string[] args)
         {
-            List<int> intList = new List<int>();
-            printEvenNumbers(ref intList);
-
+            Console.WriteLine("input: "+"11110011");
+            Console.WriteLine("output: " + BinaryStringThing("11110011"));
+            Console.WriteLine("input: "+"00001010");
+            Console.WriteLine("output: " + BinaryStringThing("00001010"));
+            Console.WriteLine("input: "+"00000101");
+            Console.WriteLine("output: " + BinaryStringThing("00000101"));
+            Console.WriteLine("input: "+"10101100");
+            Console.WriteLine("output: "+BinaryStringThing("10101100"));
             Console.Read();
-            
+
         }
     }
-    
+
 }
 

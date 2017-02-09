1bdc069bceb397f29b53b2cf662f4469a6f74e12
MaxGuidry <mguidry7@outlook.com>
2017-02-09 12:48:14 -0600

    test with singleton pattern

---
+++ Singlton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace Singlton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Donray a nignog.");
+        }
+    }
+}
